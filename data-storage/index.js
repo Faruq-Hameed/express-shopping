65b8696ad1265b98baadb7d89ea1b7c6e7eebee3
Faruq-Hameed <faruqhameed1@gmail.com>
2022-11-22 17:52:35 +0100

    first commit for shopping list app

---
+++ data-storage/index.js
@@ -0,0 +1,9 @@
+const items_array = [
+    { "name": "carrots", "id": 1, qty: 11, price: 500 },
+    { "name": "spinach", "id": 2, qty: 7 ,price: 60},
+    { "name": "cookies", "id": 3, qty: 16 ,price: 100},
+    { "name": "lettuce", "id": 4, qty: 8 ,price: 200},
+    { "name": "avocado", "id": 5, qty: 10  ,price: 340}
+];
+
+module.exports = items_array
